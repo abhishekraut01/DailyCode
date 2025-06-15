b29c8a8a8a39391a44185c9ff5659c487f8aca11
Abhishek <Abhishekraut8697@gmail.com>
2025-06-15 23:54:09 +0530

    added libuv example

--- Nodejs/LibuvExamples/index.js
+++ Nodejs/LibuvExamples/index.js
@@ -18,3 +18,44 @@ fs.readFile("file.txt", "utf-8", (err, data) => {
 process.nextTick(() => console.log("Hello from process.nexttick last vala"));
 
 console.log("sabse last wala");
+
+const fs = require("fs");
+
+setImmediate(() => console.log("execute setImmediate"));
+setTimeout(() => console.log("execute timeout 0 second wala"), 0);
+
+Promise.resolve("resolve").then((result) => console.log(result));
+
+fs.readFile("file.txt", "utf-8", (err, data) => {
+  setTimeout(() => console.log("Timer inside readfile"), 0);
+
+  process.nextTick(() => console.log("Hello from process.nexttick"));
+
+  setImmediate(() => console.log("execute setImmediate in readfile"));
+
+  console.log("file reading callback");
+});
+
+process.nextTick(() => console.log("Hello from process.nexttick last vala"));
+
+console.log("sabse last wala");
+
+
+const fs = require("fs");
+
+
+setTimeout(() => console.log("execute timeout 0 second wala"), 0);
+
+Promise.resolve("resolve").then((result) => console.log(result));
+
+fs.readFile("file.txt", "utf-8", (err, data) => {
+  setTimeout(() => console.log("Timer inside readfile"), 0);
+
+  process.nextTick(() => console.log("Hello from process.nexttick"));
+
+});
+
+process.nextTick(() => console.log("Hello from process.nexttick last vala"));
+
+console.log("sabse last wala");
+
