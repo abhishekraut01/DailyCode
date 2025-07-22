c6ed879efe8ba225bbc0a7c60d90d1597e5c550e
Abhishek <Abhishekraut8697@gmail.com>
2025-07-22 18:29:15 +0530

    fixing frontend

--- Deploy monorepo cicd/apps/web/app/page.tsx
+++ Deploy monorepo cicd/apps/web/app/page.tsx
@@ -1,13 +1,9 @@
-import  prisma  from "@repo/db/client";
 
 export default async function Home() {
-  const users = await prisma.user.findMany();
-  console.log(users);
+
   return (
     <div>
-      {users.map((user) => (
-        <div key={user.id}>{user.name}</div>
-      ))} 
+      <h1>hello world</h1>
     </div>
   );
 }
