86dc17f614c9d5070316cdda51c8ae7226383150
Abhishek <Abhishekraut8697@gmail.com>
2025-10-14 22:12:58 +0530

    add reqCount middleware intance of prom-client

--- prometheus/src/index.ts
+++ prometheus/src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express'
-import { logMiddleware } from './middleware.js'
+import { reqCountMiddleware } from './middleware.js'
 const app = express()
 
 
 
-app.get("/cpu", logMiddleware , (req, res) => {
+app.get("/cpu", (req, res) => {
     let sum = 0
     for (let i = 0; i < 10000000; i++) {
         sum += i
@@ -14,6 +14,19 @@ app.get("/cpu", logMiddleware , (req, res) => {
     })
 })
 
+app.get("/user", (req, res) => {
+
+    res.status(200).json({
+        message: "",
+        data: {
+            name: "abhishek",
+            role: "SDE2"
+        }
+    })
+    
+})
+
+app.use(reqCountMiddleware)
 app.listen(3000, () => {
     console.log(`server is listening at port 3000`)
 })
--- prometheus/src/middleware.ts
+++ prometheus/src/middleware.ts
@@ -1,8 +1,32 @@
-import type { Request , Response , NextFunction } from "express";
+import type { Request, Response, NextFunction } from "express";
+import Client from 'prom-client'
 
-export function logMiddleware(req:Request,res:Response , next:NextFunction){
+const reqCount = new Client.Counter({
+    name: 'http_requests_total',
+    help: 'Total number of http request',
+    labelNames: ['method', 'route', 'status_code']
+})
+
+export function logMiddleware(req: Request, res: Response, next: NextFunction) {
     let startTime = Date.now()
     next()
     let endTime = Date.now()
     console.log(`The time took to response is ${endTime - startTime}ms on method ${req.method} on route ${req.route.path}`)
+}
+
+export function reqCountMiddleware(req: Request, res: Response, next: NextFunction) {
+    let startTime = Date.now()
+
+    res.on('finish', () => {
+        let endTime = Date.now()
+        console.log(`req took ${endTime - startTime}ms`)
+
+        reqCount.inc({
+            method: req.method,
+            route: req.route ? req.route.path : req.path,
+            status_code: res.statusCode
+        })
+    })
+
+    next()
 }
