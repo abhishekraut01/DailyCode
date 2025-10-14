29eba24be4e2550d77426e7716e8e61843a5db2d
Abhishek <Abhishekraut8697@gmail.com>
2025-10-14 19:57:06 +0530

    fix:express code

--- prometheus/src/index.ts
+++ prometheus/src/index.ts
@@ -2,15 +2,19 @@ import express from 'express'
 const app = express()
 
 
-app.get("/cpu",(req,res)=>{
+app.get("/cpu", (req, res) => {
     let startTime = Date.now()
     let sum = 0
-    for(let i=0; i<1000000000; i++){
+    for (let i = 0; i < 1000000000; i++) {
         sum += i
     }
     let endTime = Date.now()
     res.status(200).json({
-        message:"success",
-        data:`The time took to response is ${endTime - startTime}ms on route ${req.route}`
+        message: "success",
+        data: `The time took to response is ${endTime - startTime}ms on route ${req.route}`
     })
+})
+
+app.listen(3000, () => {
+    console.log(`server is listening at port 3000`)
 })
