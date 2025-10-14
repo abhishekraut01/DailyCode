bba35303775f4a9a1807995f7e89a75f1b836ea1
Abhishek <Abhishekraut8697@gmail.com>
2025-10-14 19:55:09 +0530

    added basic express code

---
+++ prometheus/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+const app = express()
+
+
+app.get("/cpu",(req,res)=>{
+    let startTime = Date.now()
+    let sum = 0
+    for(let i=0; i<1000000000; i++){
+        sum += i
+    }
+    let endTime = Date.now()
+    res.status(200).json({
+        message:"success",
+        data:`The time took to response is ${endTime - startTime}ms on route ${req.route}`
+    })
+})
