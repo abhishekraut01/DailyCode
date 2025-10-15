8b12686d15ab6769fa3496694d3e0e4ee9e7b7f5
Abhishek <Abhishekraut8697@gmail.com>
2025-10-15 23:30:51 +0530

    added boiler plate code

--- Databases/src/index.ts
+++ Databases/src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello')
+import express from 'express'
+
+const app = express()
+
+
+
+
+
+app.post('/login', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "user created successfully",
+        data: {
+            
+        }
+    })
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is walking on port ${process.env.PORT}`)
+})
