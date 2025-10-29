17acf90bac00cd64608b3df0413ad6cd43873897
Abhishek <Abhishekraut8697@gmail.com>
2025-10-29 21:49:55 +0530

    added complex data for testing

--- Testing_in_mernstack/sample_node_app/jest.config.js
+++ Testing_in_mernstack/sample_node_app/jest.config.js
@@ -1,21 +1,3 @@
-// export default {
-//   preset: "ts-jest/presets/default-esm",
-//   testEnvironment: "node",
-//   extensionsToTreatAsEsm: [".ts"],
-//   moduleNameMapper: {
-//     "^(\\.{1,2}/.*)\\.js$": "$1"
-//   },
-//   transform: {
-//     "^.+\\.ts$": [
-//       "ts-jest",
-//       {
-//         useESM: true
-//       }
-//     ]
-//   }
-// };
-
-
 export default {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
