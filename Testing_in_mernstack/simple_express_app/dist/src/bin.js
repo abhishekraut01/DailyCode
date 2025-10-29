15218597e350b00a54222d993c84c74cd79fcb16
Abhishek <Abhishekraut8697@gmail.com>
2025-10-29 19:34:01 +0530

    setup and run express app using jest

---
+++ Testing_in_mernstack/simple_express_app/dist/src/bin.js
@@ -0,0 +1,4 @@
+import app from "./index.js";
+app.listen(3000, () => {
+    console.log("server is running like hell on 3000");
+});
--- Testing_in_mernstack/simple_express_app/dist/src/index.js
+++ Testing_in_mernstack/simple_express_app/dist/src/index.js
@@ -1,11 +1,8 @@
 import express from "express";
-
 const app = express();
 app.use(express.json());
-
 app.post("/sum", (req, res) => {
-  const { a, b } = req.body;
-  return res.json({ answer: a + b });
+    const { a, b } = req.body;
+    return res.json({ answer: a + b });
 });
-
 export default app;
