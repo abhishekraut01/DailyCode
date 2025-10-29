69aa6b35965906b6af96958aba93fd9d9930debe
Abhishek <Abhishekraut8697@gmail.com>
2025-10-29 16:59:11 +0530

    write simple test cases using jest

--- Testing_in_mernstack/simple_app_nodejs_for_test/src/index.ts
+++ Testing_in_mernstack/simple_app_nodejs_for_test/src/index.ts
@@ -1,3 +1,19 @@
-export function sum(a:number , b:number){
-    return a+b
+export function sum(a: number, b: number) {
+    return a + b
+}
+
+
+export function multiply(a: number, b: number) {
+    return a * b
+}
+
+
+export function substract(a: number, b: number) {
+    return a - b
+}
+
+
+export function devide(a: number, b: number) {
+    if (b === 0) throw Error("cant devide by zero")
+    return a / b
 }
---
+++ Testing_in_mernstack/simple_app_nodejs_for_test/src/test/index.test.ts
@@ -0,0 +1,33 @@
+import { describe, it, expect } from "@jest/globals";
+import { sum, multiply, devide, substract } from "../index.js";
+
+describe("testing all calculator functionality", () => {
+
+    describe("Test Sum funtionality", () => {
+        it("This should return 1 + 3 = 4", () => {
+            const finalAnswer = sum(1, 3)
+            expect(finalAnswer).toBe(4)
+        })
+    })
+
+    describe("Test multiply funtionality", () => {
+        it("This should return 1 * 3 = 3", () => {
+            const finalAnswer = multiply(1, 3)
+            expect(finalAnswer).toBe(3)
+        })
+    })
+
+    describe("Test substract funtionality", () => {
+        it("This should return 1 - 3 = -2", () => {
+            const finalAnswer = substract(1, 3)
+            expect(finalAnswer).toBe(-2)
+        })
+    })
+
+    describe("Test devide funtionality", () => {
+        it("This should return 4 / 2 = 2", () => {
+            const finalAnswer = devide(4, 2)
+            expect(finalAnswer).toBe(2)
+        })
+    })
+})
