e76702a328d05552ad1c8bea548fa5baa3841b9d
Abhishek <Abhishekraut8697@gmail.com>
2025-10-29 20:25:13 +0530

    setting up sample node app for esm setup with ts in min configuration

---
+++ Testing_in_mernstack/sample_node_app/src/test/index.test.ts
@@ -0,0 +1,14 @@
+import {describe, expect, test, it} from '@jest/globals';
+import request from "supertest";
+import { app } from "../index.js"
+
+describe("POST /sum", () => {
+    it("should return the sum of two numbers", async () => {
+        const res = await request(app).post("/sum").send({
+          a: 1,
+          b: 2
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.answer).toBe(3);
+      });
+});
