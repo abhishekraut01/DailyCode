ac869a8c7e446ddc65949cf0959cad09cd6a10bb
Abhishek <Abhishekraut8697@gmail.com>
2025-11-06 18:33:11 +0530

    adding interface

--- OOPS/src/interfaces/interfaces.ts
+++ OOPS/src/interfaces/interfaces.ts
@@ -1,42 +1,62 @@
-enum accountTypeEnum{
-    "savings",
-    "current"
+enum AccountTypeEnum {
+  SAVINGS = "savings",
+  CURRENT = "current"
 }
 
-interface bank{
-    accountType:accountTypeEnum
-    deposite(amount:number):void
-    withdraw(amount:number):void
-    getBalance():number
-    getUserDetails():{
-        acountHolderName:string,
-        balance:
-    }
+interface Bank {
+  accountType: AccountTypeEnum;
+  deposit(amount: number): void;
+  withdraw(amount: number): void;
+  getBalance(): number;
+  getUserDetails(): {
+    accountHolderName: string;
+    balance: number;
+    accountNumber: number;
+  };
 }
 
-class axisBank implements bank{
-    private accountType: accountTypeEnum 
-    private balance:number = 0
-    private accountHolderName: string
-    private accountNumber:number
 
-    constructor(accoutType:accountTypeEnum , accountHolderName:string){
-        this.accountType = accoutType
-        this.accountHolderName = accountHolderName
-        accountNumber
-    }
-    
-    deposite(amount: number): void {
-        this.balance += amount
-    }
+class AxisBank implements Bank {
+  private accountType: AccountTypeEnum;
+  private balance: number = 0;
+  private accountHolderName: string;
+  private accountNumber: number;
 
-    withdraw(amount: number): void {
-        this.balance -= amount
-    }
+  constructor(accountType: AccountTypeEnum, accountHolderName: string) {
+    this.accountType = accountType;
+    this.accountHolderName = accountHolderName;
+    this.accountNumber = Math.floor(Math.random() * 1_000_000);
+  }
 
-    getBalance(): number {
-        return this.balance
-    }
+  public deposit(amount: number): void {
+    this.balance += amount;
+  }
 
-    
+  public withdraw(amount: number): void {
+    if (amount > this.balance) {
+      throw new Error("Insufficient funds!");
+    }
+    this.balance -= amount;
+  }
+
+  public getBalance(): number {
+    return this.balance;
+  }
+
+  public getUserDetails() {
+    return {
+      accountNumber: this.accountNumber,
+      accountHolderName: this.accountHolderName,
+      balance: this.balance
+    };
+  }
 }
+
+
+const newUser = new AxisBank(AccountTypeEnum.CURRENT, "Abhishek");
+
+newUser.deposit(5000);
+newUser.withdraw(2000);
+
+console.log(newUser.getUserDetails());
+console.log("Current Balance:", newUser.getBalance());
