51d642eaac99fae60d309650228632be60152a7f
Abhishek <Abhishekraut8697@gmail.com>
2025-11-06 20:47:06 +0530

    added prisma instacne

--- OOPS/src/databases/index.ts
+++ OOPS/src/databases/index.ts
@@ -1,2 +1,30 @@
 import { PrismaClient } from "../Prisma/prisma.js";
 
+class PrismaSingleton {
+    private static instance: PrismaClient;
+
+    private constructor() {
+        // Private constructor ensures class cannot be instantiated directly
+    }
+
+    public static getInstance(): PrismaClient {
+        if (!PrismaSingleton.instance) {
+            PrismaSingleton.instance = new PrismaClient();
+        }
+
+        return PrismaSingleton.instance;
+    }
+}
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+    globalForPrisma.prisma ?? PrismaSingleton.getInstance();
+
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
