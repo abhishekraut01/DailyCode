80f218c74a63118770195caf6b08ca0758c1b6dd
Abhishek <Abhishekraut8697@gmail.com>
2025-11-27 01:47:40 +0530

    implement own react dom render implementation

--- React/CustomReact/app.js
+++ React/CustomReact/app.js
@@ -1,18 +1,26 @@
-function createElement(tag, attributes, children) {
-  const element = document.createElement(tag);
-  element.innerText = children;
+const React = {
+  createElement: function createElement(tag, attributes, children) {
+    const element = document.createElement(tag);
+    element.innerText = children;
 
-  for (let key in attributes) {
-    if (key === "style") {
-      Object.assign(element.style, attributes.style);
-    } else {
-      element[key] = attributes[key];
+    for (let key in attributes) {
+      if (key === "style") {
+        Object.assign(element.style, attributes.style);
+      } else {
+        element[key] = attributes[key];
+      }
     }
-  }
-  return element;
-}
+    return element;
+  },
+};
 
-const elem1 = createElement(
+const reactDOM = {
+  render: function (element, root) {
+    root.appendChild(element);
+  },
+};
+
+const elem1 = React.createElement(
   "h1",
   {
     className: "iamheader",
@@ -24,6 +32,5 @@ const elem1 = createElement(
   },
   "I am trying to create my custom react"
 );
-
-
-root.appendChild(elem1);
+const root = document.getElementById("root")
+reactDOM.render(elem1 , root)
