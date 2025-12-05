3fc9d71fe4951de99c55e9be6611326aa2c6e295
Abhishek <Abhishekraut8697@gmail.com>
2025-12-05 17:04:23 +0530

    added validation package

--- turborepo-simple-chat-app/apps/httpBackend/src/controllers/auth.controller.ts
+++ turborepo-simple-chat-app/apps/httpBackend/src/controllers/auth.controller.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express";
 import { AsyncHandler } from "../utils/asyncHandler.js";
+import {userSignUpSchema} from "@repo/validation/validation"
 
-export const signin = AsyncHandler(async (req: Request, res: Response) => {
-    
+export const singUp = AsyncHandler(async (req: Request, res: Response) => {
+    const {success , data } = userSignUpSchema.safeParse(req.body)
+
+    if(!success){
+        
+    }
 })
