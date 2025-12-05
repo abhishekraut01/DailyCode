8d2a38261eedca35cfcbf8dd83ca0830ece5fe21
Abhishek <Abhishekraut8697@gmail.com>
2025-12-05 20:28:19 +0530

    seprate uitls

--- turborepo-simple-chat-app/apps/httpBackend/src/controllers/auth.controller.ts
+++ turborepo-simple-chat-app/apps/httpBackend/src/controllers/auth.controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
-import { AsyncHandler } from "../utils/asyncHandler.js";
 import {userSignUpSchema} from "@repo/validation/validation"
 
 export const singUp = AsyncHandler(async (req: Request, res: Response) => {
     const {success , data } = userSignUpSchema.safeParse(req.body)
 
     if(!success){
-        
+        res.status(411).json(ApiResponse)
     }
+
+
 })
