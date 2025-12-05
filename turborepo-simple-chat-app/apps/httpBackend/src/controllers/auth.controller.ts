c3415fe137851338e30683ac11df22a258d2f2a1
Abhishek <Abhishekraut8697@gmail.com>
2025-12-06 00:31:05 +0530

    add global error handler and apiError Class

--- turborepo-simple-chat-app/apps/httpBackend/src/controllers/auth.controller.ts
+++ turborepo-simple-chat-app/apps/httpBackend/src/controllers/auth.controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
-import {userSignUpSchema} from "@repo/validation/validation"
+import { userSignUpSchema } from "@repo/validation/validation"
+import { AsyncHandler, ApiResponse, ApiError } from "@repo/utils/utils"
 
 export const singUp = AsyncHandler(async (req: Request, res: Response) => {
-    const {success , data } = userSignUpSchema.safeParse(req.body)
+    const { success, data, error } = userSignUpSchema.safeParse(req.body)
 
-    if(!success){
-        res.status(411).json(ApiResponse)
+    if (!success) {
+        throw new ApiError(400, 'Invalid User Input Schema', error.issues);
     }
-
-
+    
 })
