be2c84efcb3950788ebdb9193570c57628b6bbba
Abhishek <Abhishekraut8697@gmail.com>
2025-12-06 18:45:23 +0530

    taking token in ws backend

--- turborepo-simple-chat-app/apps/wsBackend/src/index.ts
+++ turborepo-simple-chat-app/apps/wsBackend/src/index.ts
@@ -1,18 +1,39 @@
 import { WebSocketServer, WebSocket } from 'ws'
+import jwt from 'jsonwebtoken'
 const PORT = process.env.PORT || "8002"
+const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET!
 
 const wss = new WebSocketServer({ port: Number(PORT) })
 
-wss.on("connection", (socket) => {
-    console.log("Client connected");
+wss.on("connection", (socket, req) => {
+    try {
+        if (!req.url) {
+            socket.close(1008, "Malformed request");
+            return;
+        }
 
-    socket.send("Welcome to room");
+        const host = req.headers.host || "localhost";
+        const url = new URL(req.url, `http://${host}`);
 
-    socket.on("message", (msg) => {
-        console.log("Client says:", msg.toString());
-    });
+        const token = url.searchParams.get("token");
 
-    socket.on("close", () => {
-        console.log("Client disconnected");
-    });
+        if (!token) {
+            socket.close(1008, "Missing token");
+            return;
+        }
+
+        const decoded = jwt.verify(token, JWT_ACCESS_SECRET)
+
+        if (!decoded || decoded.sub) {
+            socket.close(1008, "JWT_ACCESS_SECRET is missing");
+            return;
+        }
+
+
+
+        // Continue handshake...
+    } catch (err) {
+        console.error("WS parse error:", err);
+        socket.close(1011, "Internal server error");
+    }
 });
