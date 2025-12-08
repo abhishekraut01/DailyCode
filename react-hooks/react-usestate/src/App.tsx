72575efdd56995bea3b6e8a9abce40d8156af35f
Abhishek <Abhishekraut8697@gmail.com>
2025-12-08 14:44:24 +0530

    saw problem when we call api without useEffect

--- react-hooks/react-usestate/src/App.tsx
+++ react-hooks/react-usestate/src/App.tsx
@@ -1,16 +1,42 @@
 import { useState } from "react"
 
-export default function App() {
-  const [count, setCount] = useState(0)
+interface IUser {
+  login: string
+  id: string
+  node_id: string
+  avatar_url: string
+}
 
-  function handleIncrement() {
-    setCount((prev) => prev + 1)
+export default function App() {
+
+  const [users, setUsers] = useState<IUser[]>()
+
+  const fetchUsers = async () => {
+    const response = await fetch('https://api.github.com/users')
+    const data = await response.json()
+    setUsers(data)
   }
+  fetchUsers()
+
+  if (!users) return <div>Loading.......</div>
+  console.log(users)
 
   return (
-    <div>
-      <p>counter :{count}</p>
-      <button onClick={handleIncrement}> Increment</button>
+    <div style={{
+      height: "100vh",
+      width: "100vw",
+      display: "flex",
+      flexWrap: "wrap",
+      gap: "10px",
+      backgroundColor: "blueviolet",
+      padding: "20px"
+    }}>
+      <p>Premium Users</p>
+      {
+        users.map((user) => {
+          return <img src={user.avatar_url} height={100} width={100} />
+        })
+      }
     </div>
   )
 }
